d7abec20e378b4d1b921fdfcba620a60dc379477
Jenna Howieson <jenna@jennahowieson.co.uk>
2022-10-30 20:40:29 +0000

    attempted extensions

--- classes/guests.py
+++ classes/guests.py
@@ -1,7 +1,16 @@
+
+
 class Guest:
     def __init__(self, money, fav_song, status):
         self.money = money
         self.fav_song = fav_song
         self.status = status
-
     
+    def find_fave_song(self):
+        return self.fave_song
+    
+    def cheer_fav_song(self,song,guest):
+        if song.status == True and guest.status== True:
+            return "Yay, my favourite song is in the room" 
+        else:
+            return "Boo, I want my song"
--- tests/guests_tests.py
+++ tests/guests_tests.py
@@ -9,6 +9,8 @@ class TestGuests(unittest.TestCase):
         self.guest1 = Guest(15, "beyonce", False)
         self.guest2 = Guest(20, "beyonce", False)
         self.guest3 = Guest(10, "britney", False)
+        self.song1 = Song("beyonce", 3)
+        self.song2 = Song("britney", 2)
     
     def test_guest_has_money(self):
         self.assertEquals (15, self.guest1.money)
@@ -16,4 +18,8 @@ class TestGuests(unittest.TestCase):
     def test_guest_has_fave_song(self):
         self.assertEquals ("beyonce", self.guest1.fav_song)
     
-    
+    def test_cheer_fav_song(self):
+        self.bar.check_in_guest(self.guest1)
+        self.bar.check_in_song(self.song1)
+        song_to_check = self.guests.cheer_fav_song(self.song1, self.guest1)
+        self.assertEquals ("Yay, my favourite song is in the room", song_to_check)
